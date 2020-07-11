63044e46481c41520f135c1bcdb4878a8fbed51e
unknown <elliot.redhead@gmail.com>
2020-07-11 22:57:45 +0100

    Source changed on mouse events.

--- src/ImageToggleOnMouseOver.js
+++ src/ImageToggleOnMouseOver.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useRef} from "react";
 
 const ImageTogglerOnMouseOver = ({primaryImg, secondaryImg}) => {
+
+  const imageRef = useRef(null);
+
   return (
     <img 
-      onMouseOver={() => {}}
-      onMouseOut={() => {}}
+      onMouseOver={() => {imageRef.current.src = secondaryImg;}}
+      onMouseOut={() => {imageRef.current.src = primaryImg;}}
       src={primaryImg}
       alt=""
+      ref={imageRef}
     />
   );
 };
