0b2f892c7ea0967d6fba5919b8e72213145de154
unknown <elliot.redhead@gmail.com>
2020-07-11 22:43:23 +0100

    Corrected file paths.

--- pages/imageChangeOnMouseOver.js
+++ pages/imageChangeOnMouseOver.js
@@ -3,9 +3,9 @@ import React from "react";
 const ImageChangeOnMouseOver = () => {
   return (
     <div>
-      <img src="/static/bw/Speaker-187" alt="" />
+      <img src="static/speakers/bw/Speaker-187.jpg" alt="" />
       &nbsp;&nbsp;&nbsp;
-      <img src="/static/bw/Speaker-1124" alt="" />
+      <img src="static/speakers/bw/Speaker-1124.jpg" alt="" />
     </div>
   );
 };
