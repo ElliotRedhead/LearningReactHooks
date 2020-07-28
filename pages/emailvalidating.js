0cf579ce576c9b2866ee0b27d3c206e246e59412
unknown <elliot.redhead@gmail.com>
2020-07-28 21:46:37 +0100

    Routing page for nextjs.

---
+++ pages/emailvalidating.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import EmailValidatingForm from "../src/EmailValidatingForm";
+
+const emailvalidating = () => (
+  <EmailValidatingForm />
+);
+
+export default emailvalidating;
